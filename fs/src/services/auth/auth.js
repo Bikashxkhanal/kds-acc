b57fbf63c7262da150b1c1f0c5f95af2e5297e04
Bikash Khanal <khanalbikash007@gmail.com>
2026-08-01 16:25:16 +0545

    minor token bug fix

--- fs/src/services/auth/auth.js
+++ fs/src/services/auth/auth.js
@@ -7,8 +7,8 @@ const loginSysUser = async ({loginData}) => {
         
         try {
             const response = await api.post('/api/v1/sysuser/login', loginData);
-            localStorage.setItem("accessToken", response.data.accessToken);
-            localStorage.setItem("refreshToken", response.data.refreshToken);
+            localStorage.setItem("accessToken", response.data.access_token);
+            localStorage.setItem("refreshToken", response.data.refresh_token);
             // console.log(response?.data);
             return response?.data;
             
